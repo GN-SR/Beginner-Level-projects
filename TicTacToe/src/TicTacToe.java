120ee1f550c2e3f8995454fa986c12577aa985b4
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-11 18:25:08 +0530

    Setting up methods

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,2 +1,31 @@
-public class TicTacToe {
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener {
+
+    TicTacToe(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
 }
