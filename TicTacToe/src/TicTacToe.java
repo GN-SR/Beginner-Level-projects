f456d3f5e9b3a69e68c7d963cf97a5930412fe3f
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-11 18:59:08 +0530

    Creating game logic

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -45,14 +45,51 @@ public class TicTacToe implements ActionListener {
         frame.add(title_panel, BorderLayout.NORTH);
         frame.add(button_panel);
 
+        firstTurn();
     }
     @Override
     public void actionPerformed(ActionEvent e){
 
+        for (int i = 0; i < 9; i++){
+            if(e.getSource() == buttons[i]) {
+                if (player1_turn){
+                    if (buttons[i].getText() == ""){
+                        buttons[i].setForeground(new Color(255,0,0));
+                        buttons[i].setText("X");
+                        player1_turn = false;
+                        textfield.setText("O turn");
+                        check();
+                    }
+                }
+                else {
+                    if (buttons[i].getText() == ""){
+                        buttons[i].setForeground(new Color(0, 128, 128));
+                        buttons[i].setText("O");
+                        player1_turn = true;
+                        textfield.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
     }
 
     public void firstTurn(){
 
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (random.nextInt(2) == 0){
+            player1_turn = true;
+            textfield.setText("X Turn");
+        }
+        else {
+            player1_turn = false;
+            textfield.setText("O turn");
+        }
     }
 
     public void check(){
