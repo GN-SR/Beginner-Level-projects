da502305d51bd07cd7ac495cd395b66b7116d413
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-12 18:09:32 +0530

    Adding java frame and importing Calender

--- Java-Clock/src/MyFrame.java
+++ Java-Clock/src/MyFrame.java
@@ -1,4 +1,44 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.FlowLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+    String time;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Java Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
+        timeLabel = new JLabel();
+
+        this.add(timeLabel);
+        this.setVisible(true);
+
+        setTime();
+
+    }
+    public void setTime(){
+        while(true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
 }
