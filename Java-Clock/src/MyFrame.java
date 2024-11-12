285da13b2ee009253b3970d18ca455f2263b34cd
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-12 18:27:52 +0530

    Code Completion , should add more features

--- Java-Clock/src/MyFrame.java
+++ Java-Clock/src/MyFrame.java
@@ -9,10 +9,14 @@ public class MyFrame extends JFrame {
     Calendar calendar;
     SimpleDateFormat timeFormat;
     SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+    JFrame frame;
     JLabel timeLabel;
     JLabel dayLabel;
+    JLabel dateLabel;
     String time;
     String day;
+    String date;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,33 +24,47 @@ public class MyFrame extends JFrame {
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(false);
+        frame = new JFrame();
+        frame.setBackground(Color.BLACK);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
         dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
 
         timeLabel = new JLabel();
-        timeLabel.setFont(new Font("Verdana",Font.PLAIN, 50));
+        timeLabel.setFont(new Font("Verdana",Font.PLAIN, 30));
         timeLabel.setForeground(new Color(0, 99,200));
         timeLabel.setBackground(Color.BLACK);
         timeLabel.setOpaque(true);
 
         dayLabel = new JLabel();
         dayLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
+        dayLabel.setForeground(new Color(0, 99,200));
+        dayLabel.setBackground(Color.BLACK);
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Verdana", Font.PLAIN, 25));
+        dateLabel.setForeground(new Color(0, 99,200));
+        dateLabel.setBackground(Color.BLACK);
 
         this.add(timeLabel);
         this.add(dayLabel);
+        this.add(dateLabel);
         this.setVisible(true);
 
         setTime();
 
     }
-    public void setTime(){
+    public void setTime(){ //To get Current Time
         while(true) {
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
 
             day = dayFormat.format(Calendar.getInstance().getTime());
             dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
 
             try {
                 Thread.sleep(1000);
