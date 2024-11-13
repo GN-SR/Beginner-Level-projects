228ef66084460b8f4f92e2e8a637b18287fb8a41
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-13 21:30:37 +0530

    Definig all the method names needed in GamePanel.java file

--- SnakeGame/src/GameFrame.java
+++ SnakeGame/src/GameFrame.java
@@ -9,6 +9,8 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
+        this.setResizable(true);
+        this.setLocationRelativeTo(null);
 
     }
 
