3b1ac588d4d4ee7e077a957ab3d63b0b8073ad4c
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-14 21:16:33 +0530

    Starting to define methods

--- SnakeGame/src/GameFrame.java
+++ SnakeGame/src/GameFrame.java
@@ -9,7 +9,7 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
-        this.setResizable(true);
+        this.setVisible(true);
         this.setLocationRelativeTo(null);
 
     }
