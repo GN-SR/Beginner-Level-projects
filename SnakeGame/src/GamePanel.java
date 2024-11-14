62760fe7e2bdf799fedd914b3654440d518fd3c6
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-14 21:21:50 +0530

    defining draw() method

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -46,6 +46,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
         for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
             g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
         }
 
     }
