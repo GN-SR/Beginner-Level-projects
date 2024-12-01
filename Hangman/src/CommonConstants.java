b11648dc6f6a01d4ecfda5e10b0a95cc39d8a07f
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-01 19:47:25 +0530

    Defining game frame

--- Hangman/src/CommonConstants.java
+++ Hangman/src/CommonConstants.java
@@ -1,6 +1,8 @@
 import java.awt.*;
+import javax.swing.*;
 
 public class CommonConstants {
 
     public static final Dimension FRAME_SIZE = new Dimension(540, 760);
+
 }
--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class Hangman extends JFrame {
     public Hangman(){
         super("Hangman Game");
-        setSize();
+        setSize(CommonConstants.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
     }
 }
