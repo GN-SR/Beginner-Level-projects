4ba8c8406dccd589abf6eddffef88989b9428b1b
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-01 19:48:01 +0530

    Creating a new runnable

--- Hangman/src/App.java
+++ Hangman/src/App.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Hangman().setVisible(true);
+            }
+        });
 
     }
 }
