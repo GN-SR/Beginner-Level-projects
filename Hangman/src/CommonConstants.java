9e53e247b1a84c5d19454821b00aa2103ac42f0a
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-04 18:31:38 +0530

    Loading a Image

--- Hangman/src/CommonConstants.java
+++ Hangman/src/CommonConstants.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class CommonConstants {
 
     public static final String DATA_PATH = "resources/data.txt";
+    public static final String IMAGE_PATH = "resources/1.png";
 
     public static final Dimension FRAME_SIZE = new Dimension(540, 760);
 
