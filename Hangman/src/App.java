5f9dbaad4150697d14107747550c5e5b82fb8879
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-12-09 21:49:56 +0530

    Finalizing

--- Hangman/src/App.java
+++ Hangman/src/App.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
 
+        //method to call all the classes needed
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
